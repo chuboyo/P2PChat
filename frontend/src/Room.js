ce3c70d9ed4bd5dcad6c24885251759efdaabb43
chuboyo <akojichubiyojo1997@gmail.com>
2024-01-07 02:43:30 +0100

    corrected description error in Roomscreen jsx.

--- frontend/src/Room.js
+++ frontend/src/Room.js
@@ -45,7 +45,7 @@ function Room() {
             }}
           >
             You can create a room and share room name with a peer(s) to join.
-            Room names must are case sensitive and must match for peering
+            Room names are case sensitive and must match for peering
             connections.
           </p>
           <form onSubmit={(e) => submitHandler(e)}>
